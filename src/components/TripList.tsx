f9fb0c9ff052ca4198bdcb76192d31b73d34aa8d
Noelle Daley <noelledaley@users.noreply.github.com>
2024-03-31 17:53:18 -0500

    cleanup: import TRIPS from const file

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import Trip from "./Trip";
 import TripListHeader from "./TripListHeader";
 import "./TripList.css";
-import { SortByProperty } from "./consts/consts";
+import { TRIPS } from "./consts/consts";
+import type { SortByProperty } from "./consts/consts";
 
 export type TripData = {
   id: number;
@@ -20,40 +21,7 @@ const TripList = () => {
   const [sortOrder, setSortOrder] = useState<string>("descending");
 
   async function fetchTrips() {
-    return [
-      {
-        id: 1,
-        date: "June 30, 2023",
-        title: "Frog Lake Loop",
-        duration: 1,
-        miles: 36,
-        elevation: 2500,
-      },
-      {
-        id: 2,
-        date: "April 15, 2024",
-        title: "Three Sisters Wilderness",
-        duration: 3,
-        miles: 90,
-        elevation: 4700,
-      },
-      {
-        id: 3,
-        date: "August 16, 2023",
-        title: "Siletz River",
-        duration: 5,
-        miles: 120,
-        elevation: 3700,
-      },
-      {
-        id: 4,
-        date: "May 5, 2022",
-        title: "Fire and Ice",
-        duration: 2,
-        miles: 49,
-        elevation: 3000,
-      },
-    ];
+    return TRIPS;
   }
 
   useEffect(() => {
--- src/components/consts/consts.ts
+++ src/components/consts/consts.ts
@@ -1,1 +1,36 @@
 export type SortByProperty = "date" | "duration" | "miles" | "elevation";
+
+export const TRIPS = [
+  {
+    id: 1,
+    date: "June 30, 2023",
+    title: "Frog Lake Loop",
+    duration: 1,
+    miles: 36,
+    elevation: 2500,
+  },
+  {
+    id: 2,
+    date: "April 15, 2024",
+    title: "Three Sisters Wilderness",
+    duration: 3,
+    miles: 90,
+    elevation: 4700,
+  },
+  {
+    id: 3,
+    date: "August 16, 2023",
+    title: "Siletz River",
+    duration: 5,
+    miles: 120,
+    elevation: 3700,
+  },
+  {
+    id: 4,
+    date: "May 5, 2022",
+    title: "Fire and Ice",
+    duration: 2,
+    miles: 49,
+    elevation: 3000,
+  },
+];
