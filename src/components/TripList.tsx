6d9707ed1b1dfe880a1dd1e18b3f721e8bf5f96d
Noelle Daley <noelledaley@users.noreply.github.com>
2024-04-09 22:37:02 -0700

    cleanup: remove unnecessary state in TripList

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import Trip from "./Trip";
 import TripListHeader from "./TripListHeader";
 import "./TripList.css";
@@ -20,70 +20,47 @@ const TripList = () => {
   const [selectedSortBy, setSelectedSortBy] = useState<SortByProperty>("date");
   const [sortOrder, setSortOrder] = useState<string>("descending");
 
-  async function fetchTrips() {
-    return TRIPS;
-  }
-
   useEffect(() => {
-    fetchTrips().then(setTrips);
+    function fetchTrips() {
+      const data = TRIPS;
+      setTrips(data);
+    }
+    fetchTrips();
   }, []);
 
-  const onSelectFilter = (year: string) => {
-    setSelectedYear(year);
-  };
-
-  const onSelectSortBy = (sortByProperty: SortByProperty) => {
-    setSelectedSortBy(sortByProperty);
-  };
-
-  const onSortOrder = (sortOrder: string) => {
-    setSortOrder(sortOrder);
-    sortTrips(trips);
-  };
-
-  const sortTrips = (filteredTrips: TripData[]) => {
-    if (selectedSortBy === "date") {
-      if (sortOrder === "descending") {
-        return filteredTrips.sort((a: TripData, b: TripData) =>
-          a.date.localeCompare(b.date)
-        );
-      } else {
-        return filteredTrips.sort((a: TripData, b: TripData) =>
-          b.date.localeCompare(a.date)
-        );
-      }
-    }
-    if (sortOrder === "descending") {
-      return filteredTrips.sort(
-        (a: TripData, b: TripData) => b[selectedSortBy] - a[selectedSortBy]
+  const filteredAndSortedTrips = useMemo(() => {
+      const filteredTrips = trips.filter((trip) =>
+        selectedYear === "all" ? true : trip.date.includes(selectedYear)
       );
-    }
-    return filteredTrips.sort(
-      (a: TripData, b: TripData) => a[selectedSortBy] - b[selectedSortBy]
-    );
-  };
 
-  const filteredSortedTrips = sortTrips(
-    trips.filter((trip) =>
-      selectedYear === "all" ? true : trip.date.includes(selectedYear)
-    )
-  );
+      return filteredTrips.sort((a: TripData, b: TripData) => {
+        if (selectedSortBy === "date") {
+          return sortOrder === "descending"
+            ? a.date.localeCompare(b.date)
+            : b.date.localeCompare(a.date);
+        } else {
+          return sortOrder === "descending"
+            ? b[selectedSortBy] - a[selectedSortBy]
+            : a[selectedSortBy] - b[selectedSortBy];
+        }
+      });
+  }, [trips, selectedYear, selectedSortBy, sortOrder]);
+
+  const tripList = filteredAndSortedTrips.map((trip) => (
+      <Trip key={trip.id} trip={trip} />
+    ));
 
   return (
     <div>
       <TripListHeader
         selected={selectedYear}
-        onSelectFilter={onSelectFilter}
+        onSelectFilter={setSelectedYear}
         sortBy={selectedSortBy}
-        onSortBy={onSelectSortBy}
+        onSortBy={setSelectedSortBy}
         sortOrder={sortOrder}
-        onSortOrder={onSortOrder}
+        onSortOrder={setSortOrder}
       />
-      <div className="tripContainer">
-        {filteredSortedTrips.map((trip) => (
-          <Trip key={trip.id} trip={trip} />
-        ))}
-      </div>
+      <div className="tripContainer">{tripList}</div>
     </div>
   );
 };
