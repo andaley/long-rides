9f650b328f33dd2bcbd2286a3c7b0649215ef712
Noelle Daley <noelledaley@users.noreply.github.com>
2024-04-20 18:05:27 -0700

    feat: make trip data more human-readable

--- src/components/Trip.tsx
+++ src/components/Trip.tsx
@@ -10,14 +10,25 @@ type TripProps = {
 function Trip(props: TripProps) {
   const { date, title, duration, miles, elevation } = props.trip || {};
 
+  const durationHoursAndMinutes = `${Math.floor(duration / 3600)}h ${Math.floor(
+    (duration % 3600) / 60
+  )}m`;
+
+  const roundedMiles = Math.round(miles * 10) / 10;
+
+  const roundedElevation = Math.round(elevation);
+
+  const formattedDate = new Date(date).toLocaleDateString("default", { month: "short", day: "numeric", year: "numeric" });
+
+
   return (
     <>
       <Card className="trip">
-        <div>{date}</div>
+        <div>{formattedDate}</div>
         <h2>{title}</h2>
-        <div>Duration: {duration}</div>
-        <div>Miles: {miles}</div>
-        <div>Elevation: {elevation}</div>
+        <div>Duration: {durationHoursAndMinutes}</div>
+        <div>Miles: {roundedMiles}</div>
+        <div>Elevation: {roundedElevation}</div>
       </Card>
     </>
   );
