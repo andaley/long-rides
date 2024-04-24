0cef0828c68ecbe030b73829a1847bc47f0eae85
Noelle Daley <noelledaley@users.noreply.github.com>
2024-04-23 21:29:58 -0700

    cleanup: remove new trip handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import NewTrip from "./components/NewTrip";
 import TripList, { TripData } from "./components/TripList";
 import TRIPS from "./components/consts/strava.json";
 
@@ -11,17 +10,11 @@ function App() {
     setTrips(TRIPS);
   }, []);
 
-  const saveNewTripHandler = (newTrip: TripData) => {
-    setTrips((prevState) => {
-      return [...prevState, newTrip];
-    });
-  };
 
   return (
     <>
       <h1>Long Rides</h1>
       <TripList trips={trips} />
-      <NewTrip onSaveNewTrip={saveNewTripHandler} />
     </>
   );
 }
